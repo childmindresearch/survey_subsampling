cde65eb122a1228fd5fdba5340a97bb57709393a
Gregory Kiar <gkiar@local>
2024-02-16 13:56:02 -0500

    added data conversion script

---
+++ src/survey_subsampling/convert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from pyreadr import read_r
+import pandas as pd
+import pyarrow.parquet as pq
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    results = parser.parse_args()
+
+    # Load file
+    datafile = read_r(results.infile)
+
+    # Convert to dataframe
+    df = datafile[None]
+
+    # Write it out to parquet
+    df.to_parquet(results.outfile)
+
+
+if __name__ == "__main__":
+    main()
